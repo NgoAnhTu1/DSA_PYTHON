7e0408f1ccab0eda11aa7f262546af83ba053d84
NgoAnhTu1 <ngoanhtu01626734727@gmail.com>
2025-05-18 16:12:15 +0700

    Day 3

--- Day 3/isomorphic_strings.py
+++ Day 3/isomorphic_strings.py
@@ -2,17 +2,14 @@ def isomorphic_strings(s, t):
     if len(s) != len(t):
         return False
     dict_s = {}
-    word_in_t = []
-    for i in range(len(s)):
-        if  s[i] in dict_s and dict_s[s[i]] != t[i]:
-            return False
-        elif s[i] not in dict_s and t[i] in word_in_t:
-            return False
-        else:
+    dict_t = {}
+    for i in range (len(s)):
+        if s[i] not in dict_s and t[i] not in dict_t:
             dict_s[s[i]] = t[i]
-            word_in_t.append(t[i])
-    
-    return True
+            dict_t[t[i]] = s[i]
+        elif dict_s.get(s[i])  != t[i]  or dict_t.get(t[i]) != s[i]:
+            return False
+    return True 
 
 print(isomorphic_strings("egg", "add"))  
 print(isomorphic_strings("foo", "bar"))   
